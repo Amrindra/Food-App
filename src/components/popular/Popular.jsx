c8ab8274ce40ae6d3f1189467832fd62f63155f1
Amrindra <amrindraouk@yahoo.com>
2022-08-13 02:04:21 -0400

    Fetched API

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import "./Popular.css";
 
 function Popular() {
+  const getPopularAPIData = async () => {
+    const apiRes = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    const data = await apiRes.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getPopularAPIData();
+  }, []);
+
   return <div>Popular</div>;
 }
 
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./homepage/Homepage";
+
+function Pages() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default Pages;
