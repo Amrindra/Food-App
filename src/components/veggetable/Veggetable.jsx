485560393e11a45d1723e19b9ae26befe1e49beb
Amrindra <amrindraouk@yahoo.com>
2022-08-13 01:43:17 -0400

    created component directories and setup boilerplates

---
+++ src/components/veggetable/Veggetable.jsx
@@ -0,0 +1,7 @@
+import "./Veggetable.css";
+
+function Veggetable() {
+  return <div>Veggetable</div>;
+}
+
+export default Veggetable;
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Popular from "../../components/popular/Popular";
+import Veggetable from "../../components/veggetable/Veggetable";
+
+function Homepage() {
+  return (
+    <div>
+      <Veggetable />
+      <Popular />
+    </div>
+  );
+}
+
+export default Homepage;
