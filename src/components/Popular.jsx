6cda246105ac88338c4540ae3089c5b532a43081
Amrindra <amrindraouk@yahoo.com>
2022-09-07 23:48:31 -0400

    Styled the Card component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -22,9 +22,15 @@ function Popular() {
     <div>
       <Wrapper>
         <h3>Popular Picks</h3>
-
         {/* Splide is a React library for Images slider */}
-        <Splide>
+        <Splide
+          options={{
+            perPage: 4,
+            gap: "5rem",
+            drag: "free",
+            pagination: false,
+          }}
+        >
           {popularData.map((recipe) => (
             <SplideSlide>
               <Card>
@@ -46,10 +52,33 @@ const Wrapper = styled.div`
 const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
-  /* overflow: hidden; */
+  overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
