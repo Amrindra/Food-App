c592b31cde9b6927b8f384129e780128b84a9a2e
Amrindra <amrindraouk@yahoo.com>
2022-09-11 11:04:50 -0400

    Implemented the searchbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <div className="App">
+      <Searchbar />
       <Category />
       <Pages />
     </div>
