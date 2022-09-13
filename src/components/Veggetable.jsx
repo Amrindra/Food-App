cd69e97999bbe9ed40278fe4455b6a7d97f0f3d7
Amrindra <amrindraouk@yahoo.com>
2022-09-12 23:00:50 -0400

    Modified the Vegetarian card

--- src/components/Veggetable.jsx
+++ src/components/Veggetable.jsx
@@ -44,13 +44,20 @@ function Veggetable() {
           pagination: false,
         }}
       >
-        {veggie.map((recipe) => (
-          <SplideSlide key={recipe.id}>
-            <Link to={"/recipe/" + recipe.id}>
+        {veggie.map((item) => (
+          <SplideSlide key={item.id}>
+            <Link to={"/recipe/" + item.id}>
               <Card>
-                <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title} />
-                <Gradient />
+                <ImageWrapper>
+                  <img src={item.image} alt={item.title} />
+                </ImageWrapper>
+                <CardBody>
+                  <p>{item.title}</p>
+                  <div>
+                    <span>{item.pricePerServing}</span>
+                    <button>Add to cart</button>
+                  </div>
+                </CardBody>
               </Card>
             </Link>
           </SplideSlide>
@@ -69,43 +76,26 @@ const Container = styled.div`
 `;
 
 const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2px;
+  min-height: 20rem;
+  border-radius: 5px;
   overflow: hidden;
   position: relative;
+  border: 0.3rem solid gold;
 
   img {
-    position: absolute;
-    left: 0;
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
 
   p {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
     color: white;
-    width: 100%;
-    text-align: center;
     font-weight: 600;
     font-size: 1.5rem;
-    height: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
   }
 `;
 
-const Gradient = styled.div`
-  z-index: 2;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
-`;
+const ImageWrapper = styled.div``;
+const CardBody = styled.div``;
 
 export default Veggetable;
