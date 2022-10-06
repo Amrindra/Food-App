4a683d03def96e5774a398a93a7feb90c1a42c7a
Amrindra <amrindraouk@yahoo.com>
2022-10-05 20:07:59 -0400

    created useState and handlechange to handle the input fields.

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,29 +1,68 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 const Payment = () => {
+  const [cardValues, setCardValue] = useState({
+    cardHolderName: "",
+    cardNumber: "",
+    expDate: "",
+    cvv: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCardValue({ ...cardValues, [name]: value });
+    console.log(cardValues);
+  };
+
   return (
     <div>
       <PaymentForm>
         <h3>Payment</h3>
         <InputField>
           <label htmlFor="name">Card Holder's Name</label>
-          <input type="text" placeholder="Name" name="name" value="" />
+          <input
+            type="text"
+            placeholder="Name"
+            name="cardHolderName"
+            value={cardValues.cardHolderName}
+            onChange={handleChange}
+          />
         </InputField>
 
         <InputField>
           <label htmlFor="cardnumber">Card Number</label>
-          <input type="text" placeholder="Name" name="cardnumber" value="" />
+          <input
+            type="text"
+            placeholder="Name"
+            name="cardNumber"
+            value={cardValues.cardNumber}
+            onChange={handleChange}
+          />
         </InputField>
 
         <InputFieldBottom>
           <Wrapper>
             <label htmlFor="date">Expiration Date</label>
-            <input type="date" placeholder="Name" name="date" value="" />
+            <input
+              type="date"
+              placeholder="Name"
+              name="expDate"
+              value={cardValues.expDate}
+              onChange={handleChange}
+            />
           </Wrapper>
           <Wrapper>
             <label htmlFor="cvv">CVV</label>
-            <input type="text" placeholder="Code" name="cvv" value="" />
+            <input
+              type="text"
+              placeholder="Code"
+              name="cvv"
+              value={cardValues.cvv}
+              onChange={handleChange}
+              maxLength={3}
+            />
           </Wrapper>
         </InputFieldBottom>
       </PaymentForm>
